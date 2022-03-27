b6cf22bdea9921f98158a3e0942c0a5a65331d97
nikita <m_kemal@mail.ru>
2022-03-27 05:38:11 +0300

    Updates

---
+++ precache-manifest.c34ff2a37a3bf986d5a20ffd7e53ff0b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714d9b5fba6b027938a6686eb701f0ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "797539bc46c10c520aad",
+    "url": "/static/css/2.6780e105.chunk.css"
+  },
+  {
+    "revision": "26038f444f982fff74b8",
+    "url": "/static/css/main.7fe3fc86.chunk.css"
+  },
+  {
+    "revision": "797539bc46c10c520aad",
+    "url": "/static/js/2.67dd07be.chunk.js"
+  },
+  {
+    "revision": "0243a3be4cbdc65f6f010d104f1e95f9",
+    "url": "/static/js/2.67dd07be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26038f444f982fff74b8",
+    "url": "/static/js/main.3fb75331.chunk.js"
+  },
+  {
+    "revision": "73b0a4b6fd5c330b4acd",
+    "url": "/static/js/runtime-main.c32d61ac.js"
+  },
+  {
+    "revision": "7c8d991e8a2be6abd4986c3e00c35aec",
+    "url": "/static/media/facebook.7c8d991e.png"
+  },
+  {
+    "revision": "d3e319d672e63ee77d41fd9d001609ac",
+    "url": "/static/media/freeride.d3e319d6.svg"
+  },
+  {
+    "revision": "178113c8bb5c39647cad744faa021288",
+    "url": "/static/media/interval.178113c8.svg"
+  },
+  {
+    "revision": "7e9a2283a61eec5097a62b35e5fec0c6",
+    "url": "/static/media/steady.7e9a2283.svg"
+  },
+  {
+    "revision": "d1044b2527b5b74f5802cbb1fd100bd0",
+    "url": "/static/media/twitter.d1044b25.png"
+  },
+  {
+    "revision": "a32c38a203a966cc82397660187e9f88",
+    "url": "/static/media/warmdown.a32c38a2.svg"
+  },
+  {
+    "revision": "edbffcc745d194ea27157d9826264c89",
+    "url": "/static/media/warmup.edbffcc7.svg"
+  }
+]);
